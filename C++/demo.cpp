5e4e330f4809b1c7ec13d7ba82163ec608002ad6
king_badhan <pankaj4utanda@gmail.com>
2024-09-06 00:40:59 +0530

    StartLater

--- C++/demo.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "king_badhan is writing c++ language" << std::endl;
-    return 0;
-}
--- C/demo.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  printf("king_badhan write C language");
-  return 0;
-}
